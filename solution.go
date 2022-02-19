8834b01fd3c2bca5ea027e8f736ce73ae25d8014
Donich1987 <99492348+Donich1987@users.noreply.github.com>
2022-02-19 21:21:22 +0300

    Update solution.go

--- solution.go
+++ solution.go
@@ -1,27 +1,12 @@
-package donich
+package square
 
-import (
-	"math"
-)
+// Define custom int type to hold sides number and update CalcSquare signature by replacing #yourTypeNameHere#
 
-type myType int
+// Define constants to represent 0, 3 and 4 sides.  Test uses mnemos: SidesTriangle(==3), SidesSquare(==4), SidesCircle(==0)
+// it's like:
+// CalcSquare(10.0, SidesTriangle)
+// CalcSquare(10.0, SidesSquare)
+// CalcSquare(10.0, SidesCircle)
 
-func CalcSquare(sideLen float64, sidesNum myType) (res float64) {
-
-	const SidesTriangle myType = 3
-	const SidesSquare myType = 4
-	const SidesCircle myType = 0
-
-	if SidesTriangle == sidesNum {
-<<<<<<< HEAD
-		res = (math.Pow(sideLen, 2.0) * math.Sqrt(3.0)) / 4
-=======
-		res = (math.Pow(sideLen, 2.0) * math.Sqrt(3.0))/4
->>>>>>> 32b5f5b403bf722d5356d1872dfbea3f351400b3
-	} else if SidesSquare == sidesNum {
-		res = math.Pow(sideLen, 2.0)
-	} else if SidesCircle == sidesNum {
-		res = math.Pi * math.Pow(sideLen, 2.0)
-	}
-	return
+func CalcSquare(sideLen float64, sidesNum #yourTypeNameHere#) float64 {
 }
